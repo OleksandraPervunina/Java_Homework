6b2d5c008652a9a7886192979c09a4b1e87f31ba
aleksandrap <aleksandrap@wix.com>
2016-07-15 13:30:57 +0300

    4 tasks have been complited

--- src/main/java/HowManyEven.java
+++ src/main/java/HowManyEven.java
@@ -3,21 +3,6 @@
  */
 public class HowManyEven {
 
-    public static void main(String[] args) throws Exception {
 
-        for (int j = 2; j <= 20; j = j + 2) {
-            System.out.print(j + " ");
-        }
-        System.out.println();
-        for (int k = 3; k <= 20; k = k + 3) {
-            System.out.print(k + " ");
-        }
-        System.out.println();
-
-        for (int h = 4; h <= 20; h = h + 4) {
-            System.out.print(h + " ");
-        }
-
-    }
 }
 
