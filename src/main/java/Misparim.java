3fe948c587c154824fc8072fc1242e6ccc00bd0f
aleksandrap <aleksandrap@wix.com>
2016-07-15 13:30:34 +0300

    4 tasks have been complited

--- src/main/java/Misparim.java
+++ src/main/java/Misparim.java
@@ -2,4 +2,21 @@
  * Created by aleksandrap on 7/13/2016.
  */
 public class Misparim {
+
+    public static void main(String[] args) throws Exception {
+
+        for (int j = 2; j <= 20; j = j + 2) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+        for (int k = 3; k <= 20; k = k + 3) {
+            System.out.print(k + " ");
+        }
+        System.out.println();
+
+        for (int h = 4; h <= 20; h = h + 4) {
+            System.out.print(h + " ");
+        }
+
+    }
 }
