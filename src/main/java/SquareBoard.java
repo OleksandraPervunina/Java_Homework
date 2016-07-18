1527da4362f27c77388a42d740ca7aa3f91daa18
aleksandrap <aleksandrap@wix.com>
2016-07-18 16:56:53 +0300

    6 tasks have been complited

--- src/main/java/SquareBoard.java
+++ src/main/java/SquareBoard.java
@@ -1,5 +1,15 @@
 /**
- * Created by aleksandrap on 7/13/2016.
+ * Write a program called SquareBoard that displays the following n×n (n=5) pattern using two nested for loops.
+ Your program should use only two output statements,one EACH of the followings:
+ System.out.print("# "); // print # and a space, without newline
+ System.out.println(); // print a newline
+
+ Output should look like:
+ # # # # #
+ # # # # #
+ # # # # #
+ # # # # #
+ # # # # #
  */
 public class SquareBoard {
     public static void main(String[] args) {
