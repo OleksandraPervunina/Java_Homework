7d06c7dcd7c311935637933cf3a5315680f7b98c
aleksandrap <aleksandrap@wix.com>
2016-07-18 16:55:54 +0300

    6 tasks have been complited

--- src/main/java/HowManyEven.java
+++ src/main/java/HowManyEven.java
@@ -1,8 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by aleksandrap on 7/13/2016.
+ * Write a program called HowManyEven which prints how many even digits number n has (Assumption: n>0).
+ For example - per given 254698 number the program should print 4
  */
 public class HowManyEven {
 
+    public static void main(String[] args) {
+        int i = 254698;
+        int counter = 0;
 
+        while (i > 0) {
+            int digit = i % 10;
+            if (digit % 2 == 0) {
+                counter++;
+            }
+            i = i / 10;
+        }
+
+      System.out.print(counter);
+    }
 }
+
 
